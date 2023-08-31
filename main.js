1e6e2ce1a6d03331912b866b3d58b242f5662501
Samuel Jordano <eusamueljordano@gmail.com>
2023-08-31 01:13:10 -0300

    Javascript para algumas funcionabilidades

---
+++ main.js
@@ -0,0 +1,12 @@
+const navmenu = document.querySelector('.nav-menu');
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('ativo');
+    navmenu.classList.toggle('ativo');
+})
+
+window.addEventListener('scroll', function(){
+    let header = document.querySelector('.header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
